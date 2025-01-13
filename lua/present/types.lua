4690ca520db6f1c6c26e9608d29e05c9c7086b22
Jason Lunn <61010854+jasonleelunn@users.noreply.github.com>
2025-01-13 18:26:01 +0000

    refactor(types): split type definitions into separate file

---
+++ lua/present/types.lua
@@ -0,0 +1,17 @@
+---@class present.Slide
+---@field title string: The title of the slide
+---@field body string[]: The body of slide
+---@field blocks present.Block[]: A codeblock inside of a slide
+
+---@class present.Block
+---@field language string: The language of the codeblock
+---@field body string: The body of the codeblock
+
+---@class present.Config
+---@field hide_separator_in_title boolean?: TODO: add description
+---@field presentation_vim_options table?: Table of vim options to set during presentation mode, see :help option-list
+---@field separators string[]?: The list of patterns to use to find slide boundaries/titles
+
+---@class present.StartOptions
+---@field bufnr number?: Buffer number containing slides to present. Defaults to the current buffer `0`
+---@field filepath string?: The path to file containing slides to present. Takes precedence over `buffer_number`
