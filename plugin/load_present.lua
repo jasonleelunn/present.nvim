3254453e82f8194f16ad02488af3d7f4364af294
Jason Lunn <61010854+jasonleelunn@users.noreply.github.com>
2025-01-13 22:31:30 +0000

    feat(command): allow specifying a filepath with :PresentStart

--- plugin/load_present.lua
+++ plugin/load_present.lua
@@ -1,3 +1,10 @@
-vim.api.nvim_create_user_command("PresentStart", function()
-  require("present").start_presentation()
-end, {})
+vim.api.nvim_create_user_command("PresentStart", function(context)
+  local filepath = context.args
+  local start = require("present").start_presentation
+
+  if #filepath > 0 then
+    start({ filepath = filepath })
+  else
+    start()
+  end
+end, { complete = "file", nargs = "?" })
