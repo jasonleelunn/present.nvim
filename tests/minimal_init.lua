21441fef6ce5866c5198a239a7f9febcccb178c2
Jason Lunn <61010854+jasonleelunn@users.noreply.github.com>
2025-01-13 21:22:58 +0000

    build(tests): allow setting custom plenary.nvim path for testing via env var

--- tests/minimal_init.lua
+++ tests/minimal_init.lua
@@ -1,5 +1,7 @@
+local plenary_dir = vim.uv.os_getenv("PLENARY_NVIM_DIR") or "../plenary.nvim/"
+
 vim.opt.runtimepath:append(".")
-vim.opt.runtimepath:append("../plenary.nvim/")
+vim.opt.runtimepath:append(plenary_dir)
 
 vim.cmd("runtime! plugin/plenary.vim")
 vim.cmd("runtime! plugin/load_present.lua")
